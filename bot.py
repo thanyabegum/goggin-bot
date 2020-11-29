18cfecbba14bab388ae72ebab4f63cffb36caa4d
Thanya Begum <thanyab15@gmail.com>
2020-11-28 21:09:48 -0500

    Improved visuals of reminder messages

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import discord
 import os
-import sqlite3
 import asyncio
 
 from dateutil.parser import *
@@ -48,69 +47,53 @@ async def run_at(dt, coro):
     await wait_until(dt)
     return await coro
 
-with closing(sqlite3.connect('guild_config.db')) as db:
-    with closing(db.cursor()) as c:
-        c.execute('''CREATE TABLE IF NOT EXISTS events
-                     (user TEXT,
-                      name TEXT,
-                      event_date TEXT,
-                      event_time TEXT
-                      )
-                  ''')
-        c.execute('''CREATE TABLE IF NOT EXISTS eventCounter
-                      (username TEXT PRIMARY KEY UNIQUE NOT NULL,
-                       numEvents INTEGER)
-                  ''')
-
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 client = discord.Client()
 
-# Functions created to execute task at specific time
-async def wait_until(dt):
-    # sleep until the specified datetime
-    now = datetime.datetime.now()
-    await asyncio.sleep((dt - now).total_seconds())
+async def remind(author, name, date):
+	# calculates the days until the event
+	days = date - date.now()
+	hours = (days.days * 24 + days.seconds)//3600
 
-async def run_at(dt, coro):
-    await wait_until(dt)
-    return await coro
+	# converts date to appropriate string representation
+	# representation is: 0:00PM on Saturday, Oct 24 (in x hours)
+	event_time = date.strftime("%I:%M%p on %A, %B %d, %Y")
+	event_time += " (in " + str(hours) + " hours)"
 
-
-async def remind(author,name):
-	embed = discord.Embed(title="Reminder", description="This is your reminder")
-	embed.add_field(name="The Reminder", value=f"You have {name}")
+	# creates embed
+	embed = discord.Embed(
+		title=name.title() + " Reminder",
+		description="You've got an event coming up!")
+	embed.set_footer(text="P.S. STAY HARD")
+	embed.add_field(name="Event Time", value=date.strftime(event_time))
 	await author.send(content=None, embed=embed)
 
 bot = commands.Bot(command_prefix='!')
 
 @bot.command()
-async def DM(ctx, user: discord.User, *, message=None):
-    message = message or "This Message is sent via DM"
-    await user.send(message)
-
-@bot.command()
-async def create(ctx, name, ti, event_date):
+async def create(ctx, name, *, time):
 	author = ctx.message.author
-	message = f"You created a reminder for {name} at {ti} on {event_date}"
-
-	with closing(sqlite3.connect('guild_config.db')) as db:
-	    with closing(db.cursor()) as c:
-	        c.execute('''INSERT INTO events(user, name, event_date, event_time) VALUES (?, ?, ?, ?)''',
-	        (author.name, name, event_date, ti))
-	    db.commit()
-
-	date = parse(ti + " " + event_date)
-	now = datetime.datetime.now()
-	timeDelta = date - now
-
-	msg = await ctx.send(message)
-
+ 
+	# reminder emojis
 	emoji0 = '0️⃣' # when the event happens 
 	emoji1 = '1️⃣' # 10 minutes
 	emoji2 = '2️⃣' # 30 mins
 	emoji3 = '3️⃣' # 1 hour
 	emoji4 = '4️⃣' # 1 day
+
+	# creates message to react to
+	embed = discord.Embed(
+		title="Set Reminders",
+		description="When should I remind you?\n 0️⃣ At the time of the event\n 1️⃣ 10 minutes before event\n 2️⃣ 30 minutes before event\n 3️⃣ 1 hour before event\n 4️⃣ 1 day before event")
+	embed.set_footer(text="Let me know with a reaction!")
+
+	date = parse(time)
+	now = datetime.datetime.now()
+	timeDelta = date - now
+
+	msg = await ctx.send(content=None, embed=embed)
+
 	await msg.add_reaction(emoji0)
 	await msg.add_reaction(emoji1)
 	await msg.add_reaction(emoji2)
@@ -128,50 +111,38 @@ async def create(ctx, name, ti, event_date):
 				print("Emoji0 was pressed")
 				counter0 += 1
 				if counter0 == 1:
-					task0 = client.loop.create_task(run_at(date, remind(author,name)))
+					task0 = client.loop.create_task(run_at(date, remind(author,name,date)))
 
 			elif str(reaction.emoji) == emoji1:
 				print("Emoji1 was pressed")
 				counter1 += 1
 				if counter1 == 1:
 					modifiedDate = date - datetime.timedelta(minutes=10)
-					task1 = client.loop.create_task(run_at(modifiedDate, remind(author,name)))
+					task1 = client.loop.create_task(run_at(modifiedDate, remind(author,name,date)))
 
 			elif str(reaction.emoji) == emoji2:
 				print("Emoji2 was pressed")
 				counter2 += 1
 				if counter2 == 1:
 					modifiedDate = date - datetime.timedelta(minutes=30)
-					task2 = client.loop.create_task(run_at(modifiedDate, remind(author,name)))
+					task2 = client.loop.create_task(run_at(modifiedDate, remind(author,name,date)))
 
 			elif str(reaction.emoji) == emoji3:
 				print("Emoji3 was pressed")
 				counter3 += 1
 				if counter3 == 1:
 					modifiedDate = date - datetime.timedelta(hours=1)
-					task3 = client.loop.create_task(run_at(modifiedDate, remind(author,name)))
+					task3 = client.loop.create_task(run_at(modifiedDate, remind(author,name,date)))
 
 			elif str(reaction.emoji) == emoji4:
 				print("Emoji4 was pressed")
 				counter4 += 1
 				if counter4 == 1:
 					modifiedDate = date - datetime.timedelta(days=1)
-					task4 = client.loop.create_task(run_at(modifiedDate, remind(author,name)))
-			
-
-			# if str(reaction1.emoji) == emoji0:
-			# 	task0.cancel()
+					task4 = client.loop.create_task(run_at(modifiedDate, remind(author,name,date)))
 
 		except asyncio.TimeoutError:
 			print("Successfully timed out")
 			loopclose = 1
-
-# db = sqlite3.connect('guild_config.db')
-# c = db.cursor()
-# c.execute("SELECT user, event_date FROM events WHERE user=?", ("ArmyGuy741",))
-# rows = c.fetchall()
-# for row in rows:
-# 	print(row)
-
 
 bot.run(TOKEN)
